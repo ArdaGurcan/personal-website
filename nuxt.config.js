f9e3e5f2b011523085f11186ccfca8a70c07c2af
ArdaGurcan <ardagurcan2004@gmail.com>
2022-05-01 01:26:31 +0300

    update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Personal Website of Arda Gürcan',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
